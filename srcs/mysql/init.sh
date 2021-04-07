772107ec948a0b2216a008c2d669b747da777054
hyechoi <hyechoi@student.42seoul.kr>
2021-04-07 16:06:12 +0900

    Modify secret parts

--- srcs/mysql/init.sh
+++ srcs/mysql/init.sh
@@ -6,16 +6,13 @@
 #    By: hyechoi <hyechoi@student.42seoul.kr>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/07 14:51:50 by hyechoi           #+#    #+#              #
-#    Updated: 2021/04/07 15:37:51 by hyechoi          ###   ########.fr        #
+#    Updated: 2021/04/07 15:59:28 by hyechoi          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-DB_ROOT_PASSWORD="t3st1sb3tt3rth4nth1nk!@#"
-DB_WORDPRESSUSER_PASSWORD="t3st1sg00d!@#"
-
 rc-service mariadb start
 mysql --user=root <<_EOF_
-  UPDATE mysql.user SET Password=PASSWORD('${DB_ROOT_PASSWORD}') WHERE User='root';
+  UPDATE mysql.user SET Password=PASSWORD('${MYSQL_ROOT_PASSWORD}') WHERE User='root';
   DELETE FROM mysql.user WHERE User='';
   DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
   DROP DATABASE IF EXISTS test;
@@ -24,7 +21,7 @@ mysql --user=root <<_EOF_
 _EOF_
 mysql --user=root <<_EOF_
   CREATE DATABASE wordpress;
-  CREATE USER 'wordpressuser'@'localhost' IDENTIFIED BY '${DB_WORDPRESSUSER_PASSWORD}';
-  GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY '${DB_WORDPRESSUSER_PASSWORD}' WITH GRANT OPTION;
+  CREATE USER 'wordpressuser'@'localhost' IDENTIFIED BY '${MYSQL_WORDPRESSUSER_PASSWORD}';
+  GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY '${MYSQL_WORDPRESSUSER_PASSWORD}' WITH GRANT OPTION;
   FLUSH PRIVILEGES;
 _EOF_
