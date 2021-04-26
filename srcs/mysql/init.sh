e74acef1084e21222457d95a1aa1d1cee050b7ab
hyechoi <hyechoi@student.42seoul.kr>
2021-04-26 20:39:46 +0900

    Fix init.sh to make alpine:ft-mysql image available

--- srcs/mysql/init.sh
+++ srcs/mysql/init.sh
@@ -1,27 +1,23 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    init.sh                                            :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: hyechoi <hyechoi@student.42seoul.kr>       +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2021/04/07 14:51:50 by hyechoi           #+#    #+#              #
-#    Updated: 2021/04/07 15:59:28 by hyechoi          ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
+#!/bin/sh
 
-rc-service mariadb start
-mysql --user=root <<_EOF_
-  UPDATE mysql.user SET Password=PASSWORD('${MYSQL_ROOT_PASSWORD}') WHERE User='root';
-  DELETE FROM mysql.user WHERE User='';
-  DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+if [ ! -d /run/mysqld ]; then
+	mkdir -p /run/mysqld
+fi
+
+mysql_install_db --user=root --ldata=/var/lib/mysql > /dev/null
+
+/usr/bin/mysqld --user=root --bootstrap <<_EOF_
+  USE mysql;
+  FLUSH PRIVILEGES;
+  ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+  DROP USER '';
   DROP DATABASE IF EXISTS test;
   DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
   FLUSH PRIVILEGES;
-_EOF_
-mysql --user=root <<_EOF_
   CREATE DATABASE wordpress;
   CREATE USER 'wordpressuser'@'localhost' IDENTIFIED BY '${MYSQL_WORDPRESSUSER_PASSWORD}';
   GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY '${MYSQL_WORDPRESSUSER_PASSWORD}' WITH GRANT OPTION;
   FLUSH PRIVILEGES;
 _EOF_
+
+exec /usr/bin/mysqld --user=root --console
