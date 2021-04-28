9aef87bf00a9b3c7dcf24eda124d48571376d15a
hyechoi <hyechoi@student.42seoul.kr>
2021-04-28 16:25:52 +0900

    Fix mysql login issue

--- srcs/mysql/init.sh
+++ srcs/mysql/init.sh
@@ -13,18 +13,19 @@ if [ $? -eq 1 ]
 then
 	touch /var/lib/mysql/flag
 	# Exec init query with mysql-client.
-	mysql --user=root <<_EOF_
+	mysql --user=root <<EOF
 	  USE mysql;
 	  FLUSH PRIVILEGES;
-	  ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+	  ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+	  CREATE USER 'root'@'172.17.0.1' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 	  DROP DATABASE IF EXISTS test;
 	  DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 	  FLUSH PRIVILEGES;
 	  CREATE DATABASE wordpress;
-	  CREATE USER 'wordpressuser'@'localhost' IDENTIFIED BY '${MYSQL_WORDPRESSUSER_PASSWORD}';
-	  GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY '${MYSQL_WORDPRESSUSER_PASSWORD}' WITH GRANT OPTION;
+	  CREATE USER 'wordpressuser'@'172.17.0.1' IDENTIFIED BY '$MYSQL_WORDPRESSUSER_PASSWORD';
+	  GRANT ALL ON wordpress.* TO 'wordpressuser'@'172.17.0.1' IDENTIFIED BY '$MYSQL_WORDPRESSUSER_PASSWORD' WITH GRANT OPTION;
 	  FLUSH PRIVILEGES;
-_EOF_
+EOF
 fi
 
 tail -f /dev/null
