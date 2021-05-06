ff318b176c6bd6e42ecd2cb3d1314e407dfa40d1
hyechoi <hyechoi@student.42seoul.kr>
2021-05-06 09:53:45 +0900

    Refactoring alpine:ft-mysql

--- srcs/mysql/init.sh
+++ srcs/mysql/init.sh
@@ -6,9 +6,9 @@ fi
 
 mysql_install_db --user=root --datadir=/var/lib/mysql
 
-/usr/bin/mysqld --user=root --console & sleep 3
+/usr/bin/mysqld_safe --user=root --console & sleep 3
 
-ls /var/lib/mysql/flag
+ls /var/lib/mysql/flag &>/dev/null
 if [ $? -eq 1 ]
 then
 	touch /var/lib/mysql/flag
@@ -18,12 +18,13 @@ then
 	  FLUSH PRIVILEGES;
 	  ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 	  CREATE USER 'root'@'172.17.0.1' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+	  GRANT ALL ON *.* TO 'root'@'172.17.0.1';
 	  DROP DATABASE IF EXISTS test;
 	  DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 	  FLUSH PRIVILEGES;
 	  CREATE DATABASE wordpress;
 	  CREATE USER 'wordpressuser'@'172.17.0.1' IDENTIFIED BY '$MYSQL_WORDPRESSUSER_PASSWORD';
-	  GRANT ALL ON wordpress.* TO 'wordpressuser'@'172.17.0.1' IDENTIFIED BY '$MYSQL_WORDPRESSUSER_PASSWORD' WITH GRANT OPTION;
+	  GRANT ALL ON wordpress.* TO 'wordpressuser'@'172.17.0.1';
 	  FLUSH PRIVILEGES;
 EOF
 fi
